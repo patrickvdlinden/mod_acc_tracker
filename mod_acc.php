38f231abb8264b6392e1a50094c8371d16eb6950
jesper.holster <jesper.holster@indicia.nl>
2022-02-12 18:59:18 +0100

    1: Added some styling

--- mod_acc.php
+++ mod_acc.php
@@ -8,6 +8,9 @@ defined('_JEXEC') or die;
 // Include the syndicate functions only once
 require_once dirname(__FILE__) . '/helper.php';
 
+$document = JFactory::getDocument();
+$document->addStyleSheet('modules/mod_acc/css/mod_acc.css');
+
 $helper = new ModACCHelper;
 $servers = $helper->init();
 
